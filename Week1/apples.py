702ee86e2e5242a0ac2400e6afb9bbc03ee4ed94
Green1-j <joegr193@gmail.com>
2022-11-08 17:20:37 -0500

    week1

---
+++ Week1/apples.py
@@ -0,0 +1,9 @@
+applePrice= .50
+
+w=str(input("Enter your name "))
+
+print("Hi"".The cost of an Apple is", str(applePrice))
+
+y=int(input("How many apples would you like "))
+z=f"Thank you {w} for your purchase of {y} apples. The total cost of all apples is {applePrice*y}"
+print(z)
