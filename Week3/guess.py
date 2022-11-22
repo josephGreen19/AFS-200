082abc98194e4a2e117c11fe69b58667e33fd511
Green1-j <joegr193@gmail.com>
2022-11-22 16:54:35 -0500

    guessingGame

--- Week3/guess.py
+++ Week3/guess.py
@@ -1,1 +1,54 @@
-guess= "kitchen"
+secretWord = "PROFESSOR"
+copy = secretWord
+guessedLetters =[]
+wordBoard = ["_"]*len(secretWord)
+turns = 5
+#Create a function called “showBoard” that will display the current board state which is stored in the variable “wordBoard”
+def showBoard():
+    print(" ".join(wordBoard))
+
+def checkGuess(guessed,secretWord,wordBoard):
+    index = secretWord.find(guessed)
+    while index != -1:
+        if guessed in secretWord:
+            index = secretWord.find(guessed)
+            remove = "*"
+            secretWord = secretWord[:index]+ remove + secretWord[index+1:]
+            wordBoard[index] = guessed
+        else:
+            index = -1
+    return(secretWord,wordBoard)
+
+def win():
+    for x in range(0,len(wordBoard)):
+        if wordBoard[x] == "_":
+            return -1
+    return 1
+
+print("Can you guess the secret occupation? ")
+
+
+while(turns):
+    showBoard()
+    userInput = input("Guess a letter: ").upper()
+    if userInput in secretWord:
+        secretWord,wordBoard = checkGuess(userInput,secretWord,wordBoard)
+        guessedLetters.append(userInput)
+    elif userInput in secretWord:
+        print("you already guessed "+ userInput)
+        turns = turns
+    else:
+        print("That letter is not a part of this word.")
+        turns = turns -1
+        guessedLetters.append(userInput)
+    if win() == 1:
+        turns = 0
+        print("YOU WON "+ copy)
+    elif turns == 0:
+        print("Sorry you lost")
+
+    print("you have " + str(turns)+ " turns left")
+    print()
+
+
+
--- Week3/onlyevens.py
+++ Week3/onlyevens.py
@@ -1,10 +1,38 @@
-posNumber=int(input("Enter a positive number "))
+posNumber=(input("Enter a positive number "))
 
-def onlyEvens():
-    for x in range(0,posNumber+2,2):
-        print(x)
 
-if posNumber <0:
-    print(input("Invalid input. Please enter a positive number:"))
-else:
-    onlyEvens()
+def onlyEvens(posNumber):
+    if (int(posNumber)%2 == 0):
+        onlyEvens = [evens for evens in range(int(posNumber)+1)if evens %2 ==0]
+        print(onlyEvens)
+    elif (int(posNumber)%2 != 0):
+        onlyEvens = [evens for evens in range(int(posNumber)+1)if evens %2 ==0]
+        print(onlyEvens)
+
+while not (posNumber.isdigit()):
+    posNumber = input("Invalid number, please enter valid number.")
+
+onlyEvens(posNumber)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def onlyEvens(posNumber):
+ #   for x in range(0,posNumber+2,2):
+  #      print(x)
+
+#if posNumber <0:
+ #   print(input("Invalid input. Please enter a positive number:"))
+#else:
+ #   onlyEvens()
